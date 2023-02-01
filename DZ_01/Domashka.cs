72b4ca42c81a7aebac803fbf414340b53906caa1
@DanekBB777 <bekenev.daniilkpc582020@gmail.com>
2023-02-01 18:39:37 +0300

    Govnokod

---
+++ DZ_01/Domashka.cs
@@ -0,0 +1,38 @@
+﻿namespace DZ_01
+{
+    internal class Domashka
+    {
+        static void Main(string[] args)
+        {
+            //a)
+            Console.WriteLine("Введите число и единицы измерения: ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            string? c = Console.ReadLine();
+            Console.WriteLine(i + " " + c);
+            Console.WriteLine("Введите два числа: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            int k = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(m + " " + k);
+
+            Console.WriteLine();
+
+            //b)
+            Console.WriteLine("Введите a и b: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Результат:");
+            Console.WriteLine(a + " " + 1);
+            Console.WriteLine(19 + " " + b);
+
+            Console.WriteLine();
+
+            //c)
+            Console.WriteLine("Введите x и y: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Результат:");
+            Console.WriteLine(x + " " + y);
+            Console.WriteLine(5 + " " + y);
+        }
+    }
+}
